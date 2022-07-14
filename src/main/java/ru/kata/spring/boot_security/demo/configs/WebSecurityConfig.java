4c1491571700ac708549e0fe78c70e344dd4fc36
Alexey Rumyantsev <me@bpdu.ru>
2022-07-14 21:38:02 +0300

    Add UserService and UserServiceImpl

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -17,6 +18,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     public WebSecurityConfig(SuccessUserHandler successUserHandler) {
         this.successUserHandler = successUserHandler;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -1,13 +1,16 @@
 package ru.kata.spring.boot_security.demo.service;
 
+import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
 
 import java.util.List;
+import java.util.Set;
 
 public interface UserService {
     void addUser(User user);
     void deleteUser(Long id);
     User getUser(Long id);
-    void updateUser(Long id, String name, String email, Integer age);
+    void updateUser(Long id, String name, String email, Integer age,
+                    String username, String password, Set<Role> roleSet);
     List<User> getUserList();
 }
